e5bab2ac844c4f6da1fbc90c6a6728ef990fa69a
Law <168413515+Tra-Guy@users.noreply.github.com>
2024-05-04 00:23:32 +0530

    Program.cs

--- Program/Program.cs
+++ Program/Program.cs
@@ -1,1 +1,21 @@
-//This be blank for now
+using System;
+
+public static class Program 
+{         
+    public static void Main()
+    {
+        Console.WriteLine("Hello");
+     	Console.WriteLine("Would you like to use the application?");
+        string MyChoice = Console.ReadLine();
+		string MyChoicef = MyChoice.ToUpper();
+		if(Mychoicef=="YES")
+		{
+            Console.WriteLine("Ok the application is starting....");
+        }
+		else
+		{
+            Console.WriteLine("You may have given a invalid input please refer to use only yes or no in the response");
+        }
+    }
+	
+	
